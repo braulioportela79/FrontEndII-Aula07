29ac6a2d8c6786d70b8f619690f2732adf8e783c
braulioportela79 <braulioportela@outlook.com>
2022-08-16 17:42:31 -0300

    exercicio finalizado

--- js/felinesCard.js
+++ js/felinesCard.js
@@ -6,9 +6,12 @@ export default felines.forEach(e => {
     container.appendChild(card);
     card.classList.add('item');
     card.innerHTML = `
-    
     <img src="${e.imageURL}" />
     <h2>${e.title}</h2>
     <p>${e.desc}</p>
+    <div class="newImg">
+    <input type="text" id="url-new-img" placeholder="Digite a URL da nova imagem">
+    <button class="${e.title}">Alterar Imagem</button>
+    </div>
     `;
 });
--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 import { toggleTheme } from './toggleTheme.js';
 import './felinesCard.js';
+import './updateCardImg.js'
 
-const toggle = document.querySelector('input');
+const toggle = document.querySelector('#switch');
 toggle.addEventListener('click', toggleTheme);
-
-const item = document.querySelectorAll('.item');
-const animal = prompt('Escolha o animal:');
-const newImg = prompt('Insira a nova URL da imagem:');
-
-item.forEach(e => {
-    if (e.children[1].textContent == animal) {
-        e.children[0].setAttribute('src', newImg);
-    };
-});
